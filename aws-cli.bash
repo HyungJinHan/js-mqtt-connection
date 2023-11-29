b359854d36d3543305f39567120b9de355929265
HyungJinHan <han1210_36@naver.com>
2023-11-29 17:48:11 +0900

    쿼리 테스트 진행 중

--- aws-cli.bash
+++ aws-cli.bash
@@ -1,3 +1,9 @@
 aws dynamodb scan \
- --table-name oxygen --filter-expression "device_id = :d AND measure_time = :n" \
+  --table-name oxygen \
+  --filter-expression "device_id = :d AND measure_time = :n" \
+  --expression-attribute-values '{":d":{"S": "ansim01"},":n": {"N":"1701222628347"}}'
+
+aws dynamodb query \
+  --table-name oxygen \
+  --key-condition-expression "device_id = :d AND  measure_time = :n" \
   --expression-attribute-values '{":d":{"S": "ansim01"},":n": {"N":"1701222628347"}}'
--- aws_sdk_v3.js
+++ aws_sdk_v3.js
@@ -4,11 +4,14 @@ const {
   DynamoDB,
   DescribeTableCommand,
   ScanCommand,
+  QueryCommand,
 } = require("@aws-sdk/client-dynamodb");
 
 require("dotenv").config();
 
 const REGION = process.env.AWS_REGION;
+const CURRENT_TIME = new Date();
+const MS_Time = CURRENT_TIME.getTime();
 
 const listTablesCommand = async () => {
   const client = new DynamoDBClient({ region: REGION });
@@ -71,10 +74,10 @@ const scanCommand = async () => {
         S: "ansim01",
       },
       ":v1": {
-        N: "1701222628347",
+        N: `${MS_Time}`,
       },
     },
-    FilterExpression: "#n0 = :v0 AND #n1 = :v1",
+    FilterExpression: "#n0 = :v0 AND #n1 <= :v1",
     TableName: "oxygen",
   };
   const command = new ScanCommand(input);
@@ -91,7 +94,43 @@ const scanCommand = async () => {
   }
 };
 
+const queryCommand = async () => {
+  const client = new DynamoDBClient({ region: REGION });
+  const input = {
+    ExpressionAttributeNames: {
+      "#n0": "device_id",
+      "#n1": "measure_time",
+    },
+    ExpressionAttributeValues: {
+      ":v0": {
+        S: "ansim01",
+      },
+      ":v1": {
+        N: `${MS_Time}`,
+      },
+    },
+    KeyConditionExpression: "#n0 = :v0 AND #n1 < :v1",
+    TableName: "oxygen",
+  };
+  const command = new QueryCommand(input);
+
+  try {
+    const response = await client.send(command);
+    return console.log(
+      { "QueryCommand (oxygen - ansim01 / 1701222628347)": response },
+      {
+        "QueryCommand Payload (oxygen - ansim01 / 1701222628347)":
+          response.Items[0].Payload.M,
+      },
+      "\n\n"
+    );
+  } catch (err) {
+    return console.error(err);
+  }
+};
+
 listTablesCommand();
 listTable();
 describeTableCommand();
 scanCommand();
+queryCommand();
